f5b6fe7ee3e44c58adb5569a269f9da93022ea69
karina palacios <karina.spalacios@gmail.com>
2017-06-01 21:26:38 -0400

    contruyendo js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /*
  * Archivo principal de JS
  */
+/*
+ (function modal(){
+	var boxes = Array.from(document.getElementsByClassName("project"));
+	var modal = document.getElementById("work-modal");
+	var modalBody = document.getElementsByClassName('modal-body')[0];
+	var close = document.getElementById("close");
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			event.preventDefault();
+			modalBody.innerHTML = box.innerHTML;
+			modal.classList.remove("hidden");
+			close.addEventListener("click",function(){
+				modal.classList.add("hidden");
+			});
+		});		
+	});
+})();*/
